59aef7b48bfbb465975b81a3a53c1aed65982286
Govind Diwakar <sgf@sourcegoodfood.com>
2023-11-27 01:13:53 +0000

    fix: migrations

---
+++ src/migrations/1696434674697-product_category_thumbnail.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class ProductCategoryThumbnail1696434674697 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            'ALTER TABLE "product_category"' + ' ADD COLUMN "thumbnail" text'
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            'ALTER TABLE "product_category" DROP COLUMN "thumbnail"'
+        );
+    }
+}
