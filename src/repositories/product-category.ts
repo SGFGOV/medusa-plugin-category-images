f6f3f470a4ac0a88e3fe69df6c877d54faf7da0d
Govind Diwakar <sgf@sourcegoodfood.com>
2023-12-11 04:48:46 +0000

    fix: product-category-respository

---
+++ src/repositories/product-category.ts
@@ -0,0 +1,9 @@
+import { dataSource } from "@medusajs/medusa/dist/loaders/database";
+import { ProductCategory } from "../models/product-category";
+import { ProductCategoryRepository as MedusaProductCategoryRepository } from "@medusajs/medusa/dist/repositories/product-category";
+
+export const ProductCategoryRepository = dataSource.getRepository(ProductCategory).extend({
+    ...MedusaProductCategoryRepository
+});
+
+export default ProductCategoryRepository;
